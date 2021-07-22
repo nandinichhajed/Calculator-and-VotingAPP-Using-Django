17c416b1515590d0f26641572b02ee69166f0858
nandinichhajed <nandinichhajed08@gmail.com>
2021-07-22 14:28:06 +0530

    updating views for calculatorapp

--- djangoproject/calculatorapp/views.py
+++ djangoproject/calculatorapp/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index (request):
+    return render(request, 'index.html')
+
+def submitquery(request):
+    q = request.GET['query']
+    try:
+        ans = eval(q)
+        mydictionary = {
+            "q" : q,
+            "ans" : ans,
+            "error" : False,
+            "result" : True
+        }
+        return render(request,'index.html',context=mydictionary)
+    except:
+        mydictionary = {
+            "error" : True,
+            "result" : False
+
+        }
+        return render(request,'index.html',context=mydictionary)
+
