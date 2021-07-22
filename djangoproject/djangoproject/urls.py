bdf4354ec0af09faebcf59a87dd198a29826b8cb
nandinichhajed <nandinichhajed08@gmail.com>
2021-07-22 16:06:44 +0530

    updated votingapp

--- djangoproject/djangoproject/urls.py
+++ djangoproject/djangoproject/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
    path('', include('rootapp.urls')),
    path('admin/', admin.site.urls),
    path('calculatorapp',include('calculatorapp.urls')),
+   path('votingapp',include('votingapp.urls')),
 ]
